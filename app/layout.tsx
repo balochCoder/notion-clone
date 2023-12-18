e26ab7b0086d0fa551ff68982eb4d6750200d130
balochCoder <raheelahmed017@gmail.com>
2023-12-18 17:03:15 +0400

    search and settings dialog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,55 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 import { Toaster } from "sonner";
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/providers/theme-provider'
-import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Notion Clone',
-  description: 'The connected workspace where better, faster work happens.',
+  title: "Notion Clone",
+  description: "The connected workspace where better, faster work happens.",
   icons: {
     icon: [
       {
         media: "(prefers-color-scheme: light)",
         url: "/logo.svg",
-        href: "/logo.svg"
+        href: "/logo.svg",
       },
       {
         media: "(prefers-color-scheme: dark)",
         url: "/logo-dark.svg",
-        href: "/logo-dark.svg"
-      }
-    ]
-  }
-}
+        href: "/logo-dark.svg",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange storageKey='jotion-theme'>
-            <Toaster position='bottom-center'/>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="jotion-theme"
+          >
+            <Toaster position="bottom-center" />
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
       </body>
     </html>
-  )
+  );
 }
