9fb2304259f895a94b9d3a250c36d0a6b282ba7c
balochCoder <raheelahmed017@gmail.com>
2023-12-18 13:43:35 +0400

    sidebar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,14 @@ import { cn } from '@/lib/utils';
 import React from 'react'
 import { Logo } from './logo';
 import { ModeToggle } from '@/components/mode-toggle';
+import { useConvexAuth } from 'convex/react';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/spinner';
+import Link from 'next/link';
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
   return (
     <div className={cn(
@@ -14,7 +20,40 @@ export const Navbar = () => {
     )}>
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        <ModeToggle/>
+        {
+          isLoading && (
+            <Spinner />
+          )
+        }
+
+        {
+          !isAuthenticated && !isLoading && (
+            <>
+              <SignInButton mode='modal'>
+                <Button variant="ghost" size="sm">Login</Button>
+              </SignInButton>
+              <SignInButton mode='modal'>
+                <Button size="sm">Get Jotion free</Button>
+              </SignInButton>
+            </>
+          )
+        }
+
+        {
+          isAuthenticated && !isLoading && (
+            <>
+
+              <Button variant="ghost" size="sm">
+                <Link href='/documents'>
+                  Enter Jotion
+                </Link>
+              </Button>
+              <UserButton afterSignOutUrl='/' />
+
+            </>
+          )
+        }
+        <ModeToggle />
       </div>
     </div>
   )
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://massive-firefly-8.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
